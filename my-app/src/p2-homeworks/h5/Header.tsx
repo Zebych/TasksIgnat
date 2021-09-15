ce6ad91e4332d1a03367298df508b1241bc61a3e
Zebych <81613417+Zebych@users.noreply.github.com>
2021-09-15 13:58:28 +0300

    HiddenPanel.tsx -navbar

--- my-app/src/p2-homeworks/h5/Header.tsx
+++ my-app/src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <div>
+                <NavLink to={'/pre-junior'}>PRE_JUNIOR</NavLink>
+            </div>
+            <div>
+                <NavLink to={'/junior-plus'}>JUNIOR_PLUS</NavLink>
+            </div>
         </div>
     )
 }
