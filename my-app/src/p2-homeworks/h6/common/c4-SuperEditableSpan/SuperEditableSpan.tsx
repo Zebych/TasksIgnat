f5a5d92e9370a1ba308cac3a66f56d9d1502d677
Zebych <81613417+Zebych@users.noreply.github.com>
2021-09-23 11:54:29 +0300

    pencil

--- my-app/src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ my-app/src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -67,7 +67,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-                        {children || restProps.value}
+                        ✎{children || restProps.value}
                     </span>
                 )
             }
