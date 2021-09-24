2c71bb981c419f53e986424e4ff8f955b45b4f3f
Zebych <81613417+Zebych@users.noreply.github.com>
2021-09-24 19:52:56 +0300

    reducer sort tab and test

--- my-app/src/p1-main/m1-ui/u1-app/App.tsx
+++ my-app/src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
 import HW7 from '../../../p2-homeworks/h7/HW7';
+import HW8 from "../../../p2-homeworks/h8/HW8";
 
 function App() {
     return (
         <div className={s.App}>
             <HW5/>
             <HW7/>
+            <HW8/>
         </div>
     )
 }
--- my-app/src/p2-homeworks/h8/HW8.tsx
+++ my-app/src/p2-homeworks/h8/HW8.tsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW8.module.css'
 
-// export type UserType =
 
-const initialPeople = [
+ export type UserType ={
+    _id:number
+     name:string
+     age:number
+ }
+
+const initialPeople:Array<UserType> = [
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
@@ -14,16 +20,19 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<Array<UserType>>(initialPeople) // need to fix any
 
     // need to fix any
     const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+        <div key={p._id} className={s.item}>
+         <span>{p.name}</span>
+            {p.age}
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const chek18 = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
     return (
         <div>
@@ -33,10 +42,12 @@ function HW8() {
             {/*should work (должно работать)*/}
             {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div>
+                <SuperButton onClick={sortUp}>sort up</SuperButton>
+                <SuperButton onClick={sortDown}>sort down</SuperButton>
+                <SuperButton onClick={chek18}>chek 18</SuperButton>
 
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
